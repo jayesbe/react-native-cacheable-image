31d043e6ad4a8c79a9715a0c57b5c1273c0f854e
Jesse Badwal <jesse.badwal@gmail.com>
2016-04-05 20:26:26 -0400

    Initial component implementation

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # react-native-cacheable-image
 An Image component for React Native that will cache itself to disk. 
+
+## Notes
+CacheableImage understands its state. Once you define a source the first time it's state has been set. You can create another component with the same source and it will load the same cached file without having to fetch from a remote URI.
+
+However, if you happen to change the source, the original cached file will be removed and a new cached image will be created. Basically, don't change the source once you've set it unless you need to. Create a new CacheableImage component and swap if you don't want the current image to be wiped from the cache.
+
+This is beneficial in say you have a User Profile Image.  If the user changes their image, the current profile image will be removed from the cache and the new image will be saved to the cache. 
+
+This component has been tested with AWS CloudFront and as such only uses the path to the image to generate its hash. Any URL query params are ignored. 
+
+Pull Requests for enhancing this component are welcome.    
+
+## Installation
+npm i react-native-cacheable-image --save
+
+## Usage
+import CacheableImage from 'react-native-cacheable-image'
+
+## Example
+
+    <CacheableImage 
+        resizeMode="cover"
+        style={{flex: 1}}
+        source={{uri: 'http://www.foobar.com/image.jpeg'}}
+    >
+	    <CacheableImage
+            style={styles.nestedImage}
+            source={require(./someImage.jpeg)}  	
+        />
+    </CacheableImage>
+ 
+## TODO
+- add iOS support for loader
+- add Responsive Image support (using react-native-responsive-image)
+
+LEGAL DISCLAIMER
+----------------
+
+This software is published under the MIT License, which states that:
+
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+> IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+> FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+> AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+> LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+> OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+> SOFTWARE.
+    
---
+++ image.js
@@ -0,0 +1,139 @@
+
+import React, { Image, ProgressBarAndroid } from 'react-native';
+import RNFS, { DocumentDirectoryPath } from 'react-native-fs';
+
+const SHA1 = require("crypto-js/sha1");
+const URL = require('url-parse');
+
+export default
+class CacheableImage extends React.Component {
+    state: {
+        isRemote: false,
+        cachedImagePath: null,
+        downloading: false,
+        cacheable: true,
+        jobId: null
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.source != this.props.source) {
+            this._processSource(nextProps.source);
+        }
+    }
+
+	async imageDownloadBegin(info) {
+		this.setState({downloading: true, jobId: info.jobId});
+	}
+
+	async imageDownloadProgress(info) {
+        if ((info.contentLength / info.bytesWritten) == 1) {
+            this.setState({downloading: false, jobId: null});
+        }
+	}
+
+    async checkImageCache(imageUri, cachePath, cacheKey) {
+        const dirPath = DocumentDirectoryPath+'/'+cachePath;
+        const filePath = dirPath+'/'+cacheKey;
+
+        RNFS
+        .stat(filePath)
+        .then((res) => {
+        	if (res.isFile()) {
+	        	// means file exists, ie, cache-hit
+    	    	this.setState({cacheable: true, cachedImagePath: filePath});
+    	    }
+        })
+        .catch((err) => {
+        	// means file does not exist
+        	// first make sure directory exists.. then begin download
+        	RNFS
+        	.mkdir(dirPath, true)
+        	.then(() => {
+                // before we change the cachedImagePath.. if the previous cachedImagePath was set.. remove it
+                if (this.state.cacheable && this.state.cachedImagePath) {
+                    let delImagePath = this.state.cachedImagePath;
+                    RNFS
+                    .exists(delImagePath)
+                    .then((res) => { 
+                        if (res) {
+                            RNFS
+                            .unlink(delImagePath)
+                            .catch((err) => {}); 
+                        }
+                    });
+                }
+
+        		// directory exists.. begin download
+        		RNFS
+        		.downloadFile(imageUri, filePath, this.imageDownloadBegin.bind(this), this.imageDownloadProgress.bind(this))
+        		.then(() => {
+        			this.setState({cacheable: true, cachedImagePath: filePath});
+        		})
+        		.catch((err) => {
+                    this.setState({cacheable: false, cachedImagePath: null}); 
+        		})
+        		;
+        	})
+        	.catch((err) => {
+                this.setState({cacheable: false, cachedImagePath: null});
+        	})
+        });
+    }
+
+    _processSource(source) {
+        if (source !== null 
+            && typeof source === "object" 
+            && source.hasOwnProperty('uri')) 
+        { // remote
+            const url = new URL(source.uri);
+            const type = url.pathname.replace(/.*\.(.*)/, '$1');
+            const cacheKey = SHA1(url.pathname)+'.'+type;
+            
+            this.checkImageCache(source.uri, url.host, cacheKey);
+            this.setState({isRemote: true});
+        }
+        else {
+            this.setState({isRemote: false});
+        }
+    }
+
+    componentWillMount() {
+        this._processSource(this.props.source);
+    }
+
+    componentWillUnmount() {
+    	if (this.state.downloading && this.state.jobId) {
+    		RNFS.stopDownload(this.state.jobId);
+    	}
+    }
+
+    render() {
+    	if (!this.state.isRemote || !this.state.cacheable) {
+    		return this.renderLocal();
+    	}
+
+    	if (this.state.cacheable && this.state.cachedImagePath) {
+    		return this.renderCache();
+    	}
+
+        return (
+       		<ProgressBarAndroid  />
+        );
+    }
+
+    renderCache() {
+    	return (
+            <Image {...this.props} source={{uri: 'file://'+this.state.cachedImagePath}}>
+    		{this.props.children}
+    		</Image>
+		);
+    }
+
+    renderLocal() {
+    	return (
+            <Image {...this.props} >
+    		{this.props.children}
+    		</Image>
+		);
+    }
+}
